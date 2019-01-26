f8027e686ef6370810938adaf99c325b873ff30a
jorgecoder1234 <j_castellanos_narvaez@hotmail.com>
2019-01-25 21:32:14 -0600

    Updates

---
+++ precache-manifest.3dc24baa3e1a2f93665aaa0ea8dbf421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed57062f8137737b1e82",
+    "url": "/clickygamereact/static/js/runtime~main.ed57062f.js"
+  },
+  {
+    "revision": "94a7a6bc2a49d8e7fb43",
+    "url": "/clickygamereact/static/js/main.94a7a6bc.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickygamereact/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "94a7a6bc2a49d8e7fb43",
+    "url": "/clickygamereact/static/css/main.2d5becda.chunk.css"
+  },
+  {
+    "revision": "717ff9e219ebfc845c687487286fb9aa",
+    "url": "/clickygamereact/index.html"
+  }
+];
